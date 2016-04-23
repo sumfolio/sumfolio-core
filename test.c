13c984a55b94cca7bdfcbefa576c57b0a97ed223
ajv <voisine@gmail.com>
2016-04-23 10:53:00 -0700

    _BRTransactionWitnessData()

--- test.c
+++ test.c
@@ -1829,7 +1829,7 @@ int BRTransactionTests()
     size_t scriptLen = BRAddressScriptPubKey(script, sizeof(script), address.s);
     BRTransaction *tx = BRTransactionNew();
     
-    BRTransactionAddInput(tx, inHash, 0, 1, script, scriptLen, NULL, 0, NULL, 0, TXIN_SEQUENCE);
+    BRTransactionAddInput(tx, inHash, 0, 5000000000, script, scriptLen, NULL, 0, NULL, 0, TXIN_SEQUENCE);
     BRTransactionAddOutput(tx, 100000000, script, scriptLen);
     BRTransactionAddOutput(tx, 4900000000, script, scriptLen);
 
@@ -2103,7 +2103,11 @@ int BRWalletTests()
     int64_t amt;
 
     tx = BRTransactionNew();
+<<<<<<< HEAD
     BRTransactionAddInput(tx, inHash, 0, 1, inScript, inScriptLen, NULL, 0, NULL, 0, TXIN_SEQUENCE);
+=======
+    BRTransactionAddInput(tx, inHash, 0, SATOSHIS, inScript, inScriptLen, NULL, 0, NULL, 0, TXIN_SEQUENCE);
+>>>>>>> _BRTransactionWitnessData()
     BRTransactionAddOutput(tx, 740000, outScript, outScriptLen);
     BRTransactionSign(tx, 0, &k, 1);
     w = BRWalletNew(&tx, 1, mpk);
