d8f40ee6078f730bb658d7d629c08777f779c71b
Stasiu Chyczewski <schyczewski@gmail.com>
2018-09-29 14:28:59 -0400

    Update BRTransaction.h

--- BRTransaction.h
+++ BRTransaction.h
@@ -45,7 +45,7 @@ extern "C" {
 #define TXIN_SEQUENCE        UINT32_MAX  // sequence number for a finalized tx input
 
 #define SATOSHIS             100000000LL
-#define MAX_MONEY            (21000000LL*SATOSHIS)
+#define MAX_MONEY            (100000000LL*SATOSHIS)
 
 #define BR_RAND_MAX          ((RAND_MAX > 0x7fffffff) ? 0x7fffffff : RAND_MAX)
 
